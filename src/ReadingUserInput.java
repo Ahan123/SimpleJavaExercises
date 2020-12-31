f926cb2177c30a875c8f71059b0c4e1335e74dfb
ahan123 <ahanmalli123@gmail.com>
2020-12-31 20:58:15 +0530

    Adds several new methods.

--- src/ReadingUserInput.java
+++ src/ReadingUserInput.java
@@ -3,14 +3,23 @@ import java.util.Scanner;
 public class ReadingUserInput {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int counter = 0;
+        int turnCounter = 0;
+        int sum = 0;
 
-        while(counter<10) {
-
+        while(turnCounter<10) {
+            System.out.println("Please type in the " + (turnCounter+1) + "st number.");
             boolean checker = scanner.hasNextInt();
-                if (checker == false) {
+                if (checker) {
+                    int userInput = scanner.nextInt();
+                    String line = scanner.nextLine();
+                    sum+=userInput;
+                    turnCounter++;
+
+                } else {
                     System.out.println("Invalid Number");
                 }
         }
+        scanner.close();
+        System.out.println("The total sum is " + sum);
     }
 }
